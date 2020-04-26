3a821480d327f52c318b89cb42b7f19c8668f69c
kimth007kim <kimth007kim@naver.com>
2020-04-26 22:08:12 +0900

    버튼 만들기,그리고 생명과 다음라운드 구상완료

--- app.js
+++ app.js
@@ -3,6 +3,12 @@ const ctx = canvas.getContext("2d");
 const left = document.getElementById("left");
 const right = document.getElementById("right");
 
+var espeed = 2;
+var round = 1;
+var finsh = 10;
+var ex;
+var ey;
+
 var x = canvas.width / 2;
 var y = canvas.height / 2;
 
@@ -13,6 +19,13 @@ var move = 10;
 
 left.addEventListener("click", leftClicked);
 right.addEventListener("click", rightClicked);
+
+function nextRound() {
+  var count = finish;
+  if (ey == 0) {
+    count++;
+  }
+}
 
 function leftClicked() {
   x -= move;
