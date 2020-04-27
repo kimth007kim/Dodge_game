833c56bfa8a7fc1d19951d743a450f85cc769e56
kimth007kim <kimth007kim@naver.com>
2020-04-27 22:46:42 +0900

    배경이미지 추가완료 그리고 z-index찾는중

--- app.js
+++ app.js
@@ -2,12 +2,17 @@ const canvas = document.getElementById("jsCanvas");
 const ctx = canvas.getContext("2d");
 const left = document.getElementById("left");
 const right = document.getElementById("right");
+const img = document.getElementById("myImage");
+
+var round = 1; //라운드 처음에는 1
+var finsh = 10; //라운드가 끝나는 시점
 
 var espeed = 2;
-var round = 1;
-var finsh = 10;
-var ex;
-var ey;
+var max = 500;
+var rand = Math.random() * max;
+var ex = 0;
+var ey = 0;
+var erand = 0;
 
 var x = canvas.width / 2;
 var y = canvas.height / 2;
@@ -17,14 +22,24 @@ var leftPressed = false;
 
 var move = 10;
 
+ctx.globalCompositeIoperation = "source-over";
+
 left.addEventListener("click", leftClicked);
 right.addEventListener("click", rightClicked);
 
-function nextRound() {
+function roundStart() {
   var count = finish;
   if (ey == 0) {
-    count++;
+    //canvas 바깥으로 나가면
+    count--;
+    if (count == 0) {
+      round++;
+      newxtRound();
+    }
   }
+}
+function newxtRound(round) {
+  espeed = round * espeed;
 }
 
 function leftClicked() {
@@ -41,8 +56,13 @@ function drawBall() {
   ctx.closePath();
 }
 
+function drawBackgorund() {
+  ctx.drawImage(img, 0, 0, 100, 100);
+}
+
 function draw() {
   ctx.clearRect(0, 0, canvas.width, canvas.height);
   drawBall();
+  drawBackgorund();
 }
 setInterval(draw, 10);
---
+++ readme.txt
@@ -0,0 +1,5 @@
+배경화면 만들기
+
+void ctx.drawImage(image, dx, dy);
+void ctx.drawImage(image, dx, dy, dWidth, dHeight);
+void ctx.drawImage(image, sx, sy, sWidth, sHeight, dx, dy, dWidth, dHeight);
