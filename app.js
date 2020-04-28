c88fe61f2ffd032daf1d87e2f450a807a167aee0
kimth007kim <kimth007kim@naver.com>
2020-04-28 23:00:06 +0900

    배열 작업중

--- app.js
+++ app.js
@@ -2,67 +2,198 @@ const canvas = document.getElementById("jsCanvas");
 const ctx = canvas.getContext("2d");
 const left = document.getElementById("left");
 const right = document.getElementById("right");
+const topb = document.getElementById("topb");
+const bottomb = document.getElementById("bottomb");
 const img = document.getElementById("myImage");
 
 var round = 1; //라운드 처음에는 1
-var finsh = 10; //라운드가 끝나는 시점
-
-var espeed = 2;
-var max = 500;
-var rand = Math.random() * max;
-var ex = 0;
-var ey = 0;
-var erand = 0;
+var finish = 10; //라운드가 끝나는 시점
+var status = 0;
 
 var x = canvas.width / 2;
 var y = canvas.height / 2;
+var mewidth = 20;
+var meheight = 20;
+
+//테스트용
+var tx = 10;
+var ty = 10;
+var theight = 10;
+var twidth = 10;
+var tspeed = 1;
+var startPoint = 0;
+
+var ex = 0;
+var ey = 0;
+var ewidth = 0;
+var eheight = 0;
+var espeed = 2;
+
+var max = 500; //500중에 하나의
+var rand = Math.random() * 1.0; //난수 생성
+var Rint = 0;
+
+var erand = 0;
+
+var efish = [];
+for (var c = 0; c < round; c++) {
+  var randwh = Math.floor(Math.random() * 10) + 1; //(int)적의 길이와 높이를 랜덤으로 받아온다.
+  var randx = Math.floor(Math.random()) + 1; //(int)적의 x축 좌표를 0~10의 숫자중 랜덤으로 받아온다 받아온숫자중 0~5이면 0 5이상이면 1을으로 스트타포인트를 지목
+  // var randy = Math.random() * canvas.height; //(int) y축의 좌표를 랜덤으로 받아와야한다.
+  // var randspeed = Math.random() * 3; //(float) 물고기의 속도를 0~1.0중의 속도로 받아온다.
+
+  efish[c] = {
+    twidth: randwh,
+    theight: randwh,
+    tx: td,
+    ty: Math.random() * canvas.height, //(int) y축의 좌표를 랜덤으로 받아와야한다.
+    status: 1,
+    tspeed: Math.random() * 3,
+    startPoint: 0,
+  };
+  if (randx < 5) {
+    // efish[c].tx = 290;
+    var td = 290;
+    startPoint = 1;
+  } else if (randx >= 5) {
+    // efish[c].tx = 0;
+    // var td = efish[c].tx;
+    var td = 0;
+    startPoint = 0;
+  }
+  console.log(efish[c]);
+}
 
 var rightPressed = false;
 var leftPressed = false;
+var topPressed = false;
+var bottomPressed = false;
 
 var move = 10;
 
-ctx.globalCompositeIoperation = "source-over";
+// ctx.globalCompositeIoperation = "source-over";
 
 left.addEventListener("click", leftClicked);
 right.addEventListener("click", rightClicked);
+topb.addEventListener("click", topClicked);
+bottomb.addEventListener("click", bottomClicked);
 
-function roundStart() {
-  var count = finish;
-  if (ey == 0) {
-    //canvas 바깥으로 나가면
-    count--;
-    if (count == 0) {
-      round++;
-      newxtRound();
-    }
+document.addEventListener("keydown", keyDownHandler, false);
+document.addEventListener("keyup", keyUpHandler, false);
+
+// function roundStart() {
+//   var count = finish;
+//   if (ey == 0) {
+//     //canvas 바깥으로 나가면
+//     count--;
+//     if (count == 0) {
+//       round++;
+//       newxtRound();
+//     }
+//   }
+// }
+// function newxtRound(round) {
+//   espeed = round * espeed;
+// }
+
+function keyDownHandler(e) {
+  if (e.keyCode == 37) {
+    leftPressed = true;
+  } else if (e.keyCode == 39) {
+    rightPressed = true;
+  } else if (e.keyCode == 38) {
+    topPressed = true;
+  } else if (e.keyCode == 40) {
+    bottomPressed = true;
   }
 }
-function newxtRound(round) {
-  espeed = round * espeed;
-}
 
+function keyUpHandler(e) {
+  if (e.keyCode == 37) {
+    leftPressed = false;
+  } else if (e.keyCode == 39) {
+    rightPressed = false;
+  } else if (e.keyCode == 38) {
+    topPressed = false;
+  } else if (e.keyCode == 40) {
+    bottomPressed = false;
+  }
+}
 function leftClicked() {
   x -= move;
 }
 function rightClicked() {
   x += move;
 }
+function topClicked() {
+  y -= move;
+}
+function bottomClicked() {
+  y += move;
+}
+
 function drawBall() {
   ctx.beginPath();
-  ctx.fillRect(x, y, 10, 10);
+  ctx.rect(x, y, mewidth, meheight);
   ctx.fillStyle = "yellow";
   ctx.fill();
   ctx.closePath();
 }
 
+function enemyDraw() {
+  for (var c = 0; c < round; c++) {
+    var efishX = efish[c].tx;
+    var efishY = efish[c].ty;
+    var efishWidth = efish[c].twidth;
+    var efishHeight = efish[c].theight;
+    ctx.beginPath();
+    ctx.rect(efishX, efishY, efishWidth, efishHeight);
+    var random = "#" + Math.round(Math.random() * 0xffffff).toString(16);
+    ctx.fillStyle = random;
+    ctx.fill();
+    ctx.closePath();
+  }
+}
+
 function drawBackgorund() {
-  ctx.drawImage(img, 0, 0, 100, 100);
+  ctx.drawImage(img, 0, 0, 600, 420);
+}
+
+function collisionDetection() {
+  if (x > tx && x < tx + twidth && y > ty && y < ty + theight) {
+    gameover();
+  }
+}
+function gameover() {
+  alert("Game OVER");
+  document.location.reload();
 }
 
 function draw() {
   ctx.clearRect(0, 0, canvas.width, canvas.height);
-  drawBall();
   drawBackgorund();
+  drawBall();
+  // enemyMove();
+  enemyDraw();
+  collisionDetection();
+
+  if (rightPressed && x < canvas.width - mewidth) {
+    x += 3;
+  } else if (leftPressed && x > 0) {
+    x -= 3;
+  } else if (topPressed && y > 0) {
+    y -= 3;
+  } else if (bottomPressed && y < canvas.height - meheight) {
+    y += 3;
+  }
+  for (var c = 0; c < round; c++) {
+    var estartPoint = efish[c].tx;
+    console.log(estartPoint, efish[c].ty);
+    if (estartPoint == 290) {
+      efish[c].tx += efish[c].tspeed;
+    } else if (estartPoint == 0) {
+      efish[c].tx -= efish[c].tspeed;
+    }
+  }
 }
 setInterval(draw, 10);
--- readme.txt
+++ readme.txt
@@ -1,5 +1,45 @@
-배경화면 만들기
+1.배경화면 만들기-0
 
 void ctx.drawImage(image, dx, dy);
 void ctx.drawImage(image, dx, dy, dWidth, dHeight);
 void ctx.drawImage(image, sx, sy, sWidth, sHeight, dx, dy, dWidth, dHeight);
+
+
+
+2. bottompressed가 동작하지 않는것 해결-0
+	1)e.keyCode에서 c가 소문자여서 안됐었음(스펠링)
+
+3.적 추가
+
+1) 1라운드에는 1(라운드수)*10(나오는양) 으로 해서 배열을 생성한다
+배열의 값에는 x좌표,y좌표, width,height, speed 값이 필요하다.
+한번 생성되면 일직선으로 x축을따라 지나간다.
+
+
+round: 라운드수
+finish:나오는 양
+ex:적의 x좌표
+ey: 적의 y 좌표
+ewidth:적의 크기
+eheight: 적의 길이
+espeed:적의 속도
+
+1-1)배열만들기
+빈배열을 만드는 방법
+
+ var bricks = [];
+      for (var c = 0; c < brickColumnCount; c++) {
+        bricks[c] = [];
+          bricks[c][r] = { x: 0, y: 0, status: 1 };
+        }
+
+1-2)그냥 적하나 추가해서 일자로 쭉가게 만들어보기-0
+
+4:점수추가
+
+
+
+4. 글자 반짝이게 하기
+
+
+
