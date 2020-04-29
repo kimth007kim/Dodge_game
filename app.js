cb0101d8519a9f0166581f1f65f4d6afe5e4fd29
kimth007kim <kimth007kim@naver.com>
2020-04-29 20:01:05 +0900

    적 물고기 양쪽 사이드에 배치하는것 성공! 어떻게 따로따로 나오게 할것인지 고민중

--- app.js
+++ app.js
@@ -6,14 +6,15 @@ const topb = document.getElementById("topb");
 const bottomb = document.getElementById("bottomb");
 const img = document.getElementById("myImage");
 
-var round = 1; //라운드 처음에는 1
-var finish = 10; //라운드가 끝나는 시점
+var round = 3; //라운드 처음에는 1
+var finish = 5; //라운드가 끝나는 시점
 var status = 0;
 
 var x = canvas.width / 2;
 var y = canvas.height / 2;
-var mewidth = 20;
-var meheight = 20;
+var first = 20;
+var mewidth = first;
+var meheight = first;
 
 //테스트용
 var tx = 10;
@@ -23,47 +24,81 @@ var twidth = 10;
 var tspeed = 1;
 var startPoint = 0;
 
-var ex = 0;
-var ey = 0;
-var ewidth = 0;
-var eheight = 0;
-var espeed = 2;
-
 var max = 500; //500중에 하나의
 var rand = Math.random() * 1.0; //난수 생성
 var Rint = 0;
-
 var erand = 0;
+
+// var efish = [];
+// for (var c = 0; c < round; c++) {
+//   efish[c] = [];
+//   for (var r = 0; r < finish; r++) {
+//     var randwh = Math.floor(Math.random() * 20) + 1;
+//     var stp = Math.floor(Math.random() * 2) + 1;
+//     var yMaker = Math.random() * canvas.height - randwh; //(int) y축의 좌표를 랜덤으로 받아와야한다.
+//     var speedMaker = Math.random() * 5;
+//     if (stp == 1) {
+//       var xMaker = 0;
+//     } else if (stp == 2) {
+//       var xMaker = canvas.width - randwh;
+//     }
+//     efish[c][r] = {
+//       twidth: randwh,
+//       theight: randwh,
+//       tx: xMaker,
+//       ty: yMaker,
+//       status: 1,
+//       tspeed: speedMaker,
+//       startPoint: stp,
+//       tcolor: "#" + Math.round(Math.random() * 0xffffff).toString(16),
+//     };
+//   }
+// }
 
 var efish = [];
 for (var c = 0; c < round; c++) {
-  var randwh = Math.floor(Math.random() * 10) + 1; //(int)적의 길이와 높이를 랜덤으로 받아온다.
-  var randx = Math.floor(Math.random()) + 1; //(int)적의 x축 좌표를 0~10의 숫자중 랜덤으로 받아온다 받아온숫자중 0~5이면 0 5이상이면 1을으로 스트타포인트를 지목
-  // var randy = Math.random() * canvas.height; //(int) y축의 좌표를 랜덤으로 받아와야한다.
-  // var randspeed = Math.random() * 3; //(float) 물고기의 속도를 0~1.0중의 속도로 받아온다.
-
-  efish[c] = {
-    twidth: randwh,
-    theight: randwh,
-    tx: td,
-    ty: Math.random() * canvas.height, //(int) y축의 좌표를 랜덤으로 받아와야한다.
-    status: 1,
-    tspeed: Math.random() * 3,
-    startPoint: 0,
-  };
-  if (randx < 5) {
-    // efish[c].tx = 290;
-    var td = 290;
-    startPoint = 1;
-  } else if (randx >= 5) {
-    // efish[c].tx = 0;
-    // var td = efish[c].tx;
-    var td = 0;
-    startPoint = 0;
+  efish[c] = [];
+  for (var r = 0; r < finish; r++) {
+    efish[c][r] = {
+      twidth: 0,
+      theight: 0,
+      tx: 0,
+      ty: 0,
+      status: 1,
+      tspeed: 0,
+      startPoint: 0,
+      tcolor: 0,
+    };
   }
-  console.log(efish[c]);
 }
 
+function MakeEnemy() {
+  for (var c = 0; c < round; c++) {
+    for (var r = 0; r < finish; r++) {
+      var f = efish[c][r];
+      var randwh = Math.floor(Math.random() * 20) + 1;
+      var stp = Math.floor(Math.random() * 2) + 1;
+      var yMaker = Math.random() * canvas.height - randwh; //(int) y축의 좌표를 랜덤으로 받아와야한다.
+      var speedMaker = Math.random() * 5;
+      if (stp == 1) {
+        var xMaker = 0;
+      } else if (stp == 2) {
+        var xMaker = canvas.width - randwh;
+      }
+      efish[c][r] = {
+        twidth: randwh,
+        theight: randwh,
+        tx: xMaker,
+        ty: yMaker,
+        status: 1,
+        tspeed: speedMaker,
+        startPoint: stp,
+        tcolor: "#" + Math.round(Math.random() * 0xffffff).toString(16),
+      };
+      // alert(efish[c]);
+    }
+  }
+}
 var rightPressed = false;
 var leftPressed = false;
 var topPressed = false;
@@ -80,21 +115,6 @@ bottomb.addEventListener("click", bottomClicked);
 
 document.addEventListener("keydown", keyDownHandler, false);
 document.addEventListener("keyup", keyUpHandler, false);
-
-// function roundStart() {
-//   var count = finish;
-//   if (ey == 0) {
-//     //canvas 바깥으로 나가면
-//     count--;
-//     if (count == 0) {
-//       round++;
-//       newxtRound();
-//     }
-//   }
-// }
-// function newxtRound(round) {
-//   espeed = round * espeed;
-// }
 
 function keyDownHandler(e) {
   if (e.keyCode == 37) {
@@ -142,26 +162,57 @@ function drawBall() {
 
 function enemyDraw() {
   for (var c = 0; c < round; c++) {
-    var efishX = efish[c].tx;
-    var efishY = efish[c].ty;
-    var efishWidth = efish[c].twidth;
-    var efishHeight = efish[c].theight;
-    ctx.beginPath();
-    ctx.rect(efishX, efishY, efishWidth, efishHeight);
-    var random = "#" + Math.round(Math.random() * 0xffffff).toString(16);
-    ctx.fillStyle = random;
-    ctx.fill();
-    ctx.closePath();
+    for (var r = 0; r < finish; r++) {
+      if (efish[c][r].status == 1) var f = efish[c][r];
+      var efishColor = f.tcolor;
+      var efishX = f.tx;
+      var efishY = f.ty;
+      var efishWidth = f.twidth;
+      var efishHeight = f.theight;
+      ctx.beginPath();
+      ctx.rect(efishX, efishY, efishWidth, efishHeight);
+      // var random = "#" + Math.round(Math.random() * 0xffffff).toString(16);
+      ctx.fillStyle = efishColor;
+      ctx.fill();
+      ctx.closePath();
+    }
   }
 }
-
+function enemyMove() {
+  for (var c = 0; c < round; c++) {
+    for (var r = 0; r < finish; r++) {
+      var f = efish[c][r];
+      if (f.startPoint == 1) {
+        f.tx += tspeed;
+      } else if (f.startPoint == 2) {
+        f.tx -= tspeed;
+      }
+    }
+  }
+}
 function drawBackgorund() {
   ctx.drawImage(img, 0, 0, 600, 420);
 }
 
 function collisionDetection() {
-  if (x > tx && x < tx + twidth && y > ty && y < ty + theight) {
-    gameover();
+  for (var c = 0; c < round; c++) {
+    for (var r = 0; r < finish; r++) {
+      var f = efish[c][r];
+      if (
+        f.status == 1 &&
+        x > f.tx &&
+        x < f.tx + f.twidth &&
+        y > f.ty &&
+        y < f.ty + f.theight
+      ) {
+        if (first >= f.randwh) {
+          fisrt + 2;
+          f.status == 0;
+        } else {
+          gameover();
+        }
+      }
+    }
   }
 }
 function gameover() {
@@ -169,12 +220,31 @@ function gameover() {
   document.location.reload();
 }
 
+function getRandSec() {
+  return Math.floor(Math.random() * 120) + 1;
+} //그리고 2분마다 실행하는 인터벌 함수를 정의한다.
+
+// var _interval = setInterval(targetFunction, 120000);
+
+//실행할 함수 내에서 setTimeout 으로 실행하되 시간 값을 랜덤으로 준다.
+
+// function targetFunction() {
+//   var sec = getRandSec();
+//   console.log(sec);
+//   if (_autoStartStatus) {
+//     setTimeout(function () {
+//       //여기가 실제로 실행되는 비지니스 로직.
+//     }, sec * 1000);
+//   }
+// }
+
 function draw() {
   ctx.clearRect(0, 0, canvas.width, canvas.height);
   drawBackgorund();
   drawBall();
-  // enemyMove();
   enemyDraw();
+  enemyMove();
+
   collisionDetection();
 
   if (rightPressed && x < canvas.width - mewidth) {
@@ -186,14 +256,5 @@ function draw() {
   } else if (bottomPressed && y < canvas.height - meheight) {
     y += 3;
   }
-  for (var c = 0; c < round; c++) {
-    var estartPoint = efish[c].tx;
-    console.log(estartPoint, efish[c].ty);
-    if (estartPoint == 290) {
-      efish[c].tx += efish[c].tspeed;
-    } else if (estartPoint == 0) {
-      efish[c].tx -= efish[c].tspeed;
-    }
-  }
 }
-setInterval(draw, 10);
+setInterval(draw, 100);
